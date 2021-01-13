0bc814c9ee68c340debdc72dce6bd1ffee591b02
huan <87020@126.com>
2021-01-13 10:12:36 +0800

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login_container">登录组件</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.log_contaainer {
+  background-color: #2b4b6b;
+}
+</style>
