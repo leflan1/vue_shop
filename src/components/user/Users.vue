7865d41e5b4ed2a4c022598b169b8f9d60a490a4
index <leflan1@users.noreply.github.com>
2021-01-16 11:12:18 +0800

    main

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -11,16 +11,116 @@
       <el-row :gutter="20">
         <!-- 搜索 -->
         <el-col :span="7">
-          <el-input placeholder="请输入内容" v-model="quertInfo.query">
-            <el-button slot="append" icon="el-icon-search"> </el-button>
+          <el-input
+            placeholder="请输入内容"
+            v-model="quertInfo.query"
+            clearable
+            @click="getUserList()"
+          >
+            <el-button slot="append" icon="el-icon-search" @click="getUserList">
+            </el-button>
           </el-input>
         </el-col>
         <!-- 添加按钮 -->
         <el-col :span="4">
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="addDialogVisible = true">
+            添加用户
+          </el-button>
         </el-col>
       </el-row>
+
+      <!-- 表格 -->
+      <el-table :data="userlist" border>
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态">
+          <!-- 作用域插槽来使用mg_state -->
+          <!-- 指定作用域插槽prop将会被覆盖，不再生效 -->
+          <template slot-scope="scope">
+            <!-- 按钮 -->
+            <el-switch
+              v-model="scope.row.mg_state"
+              @change="userStateChanged(scope.row)"
+            >
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" prop="username" width="180px">
+          <template slot-scope="scope">
+            <!-- 修改 -->
+            <el-button
+              type="primary"
+              size="mini"
+              icon="el-icon-edit"
+            ></el-button>
+            <!-- 删除 -->
+            <el-button
+              type="danger"
+              size="mini"
+              icon="el-icon-delete"
+            ></el-button>
+            <!-- 分配角色 -->
+            <el-tooltip
+              effect="dark"
+              content="分配角色"
+              placement="top"
+              :enterable="false"
+            >
+              <el-button
+                type="warnng"
+                size="mini"
+                icon="el-icon-setting"
+              ></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="quertInfo.pagenum"
+        :page-sizes="[2, 4, 8, 16]"
+        :page-size="quertInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
+
+    <!-- 用户对话框 -->
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%">
+      <!-- 对话框----内容主体 -->
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="70px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 对话框----内容主体 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addDialogVisible = false">
+          确 定
+        </el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -34,6 +134,66 @@ export default {
         pagenum: 1,
         pagesize: 2,
       },
+      //   用户数据列表
+      userlist: [],
+      //   总数据条数
+      total: 0,
+      // 控制添加用户对话框的现实与隐藏
+      addDialogVisible: false,
+      addForm: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: "",
+      },
+      addFormRules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur",
+          },
+          {
+            min: 3,
+            max: 10,
+            message: "用户名的长度在3~10和字符之间",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur",
+          },
+          {
+            min: 6,
+            max: 15,
+            message: "密码的长度在6~15和字符之间",
+            trigger: "blur",
+          },
+        ],
+        email: [
+          {
+            required: true,
+            message: "请输入邮箱",
+            trigger: "blur",
+          },
+        ],
+        mobile: [
+          {
+            required: true,
+            message: "请输入手机号码",
+            trigger: "blur",
+          },
+          {
+            min: 11,
+            max: 11,
+            message: "密码的长度在11位字符之间",
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
   //   生命周期函数
@@ -46,10 +206,40 @@ export default {
         params: this.quertInfo,
       });
       console.log(res);
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取用户列表失败");
+      }
+      this.userlist = res.data.users;
+      this.total = res.data.total;
+    },
+    // 监听pagesize改变的事件
+    handleSizeChange(newSize) {
+      this.quertInfo.pagesize = newSize;
+      this.getUserList();
+    },
+    // 页码值跳转
+    handleCurrentChange(newPage) {
+      this.quertInfo.pagenum = newPage;
+      this.getUserList();
+    },
+    // 更新状态
+    async userStateChanged(userinfo) {
+      console.log(userinfo);
+      const { data: res } = await this.$http.put(
+        `users/${userinfo.id}/state/${userinfo.mg_state}`
+      );
+      if (res.meta.status !== 200) {
+        userinfo.mg_state = !userinfo.mg_state;
+        return this.$message.error("更新用户状态失败!");
+      }
+      this.$message.success("更新用户状态成功！");
     },
   },
 };
 </script>
 
 <style lang="less" scoped>
+.el-pagination {
+  margin-top: 15px;
+}
 </style>
